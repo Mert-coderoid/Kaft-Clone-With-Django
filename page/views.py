59a0a22ffd06cdd2108c96a3017f647bac516679
Mert-coderoid <nevi.elric@gmail.com>
2021-11-04 22:58:04 +0300

    çalıştı

--- page/views.py
+++ page/views.py
@@ -16,45 +16,39 @@ def index(request):
     return render(request, "home/index.html", context)
 
     # Admin:
+def carousel_list(request):
+    context = dict()
+    # kaft_clone.com/manage/carousel/1/edit
+    context["carousel"] = Carousel.objects.all().order_by("-pk")
+    return render(request, "manage/carousel_list.html", context)
+
+def carousel_update(request, pk):
+        # Bu ismi oluşturun...
+    context = dict()
+        # kaft_clone/manage/carousel/1/edit
+    # item = Carousel.objects.first()
+    # context["form"] = CarouselModelForm(instance=item)
+    context = Carousel.objects.get(pk=pk)
+    return render(request, "manage/corousel_form.html", context)
+
 def carousel_create(request):
     context = dict()
-    context["form"] = CarouselModelForm
-    item = Carousel.objects.first()
-    context["form"] = CarouselModelForm(instance=item)
+    context["form"] = CarouselModelForm()
+    # item = Carousel.objects.first()
+    # context["form"] = CarouselModelForm(instance=item)
 
     if request.method == "POST":
         print(request.POST)
-        # print(request.FILES["cover_image"])
-        
+        print(request.FILES.get("cover_image"))
+
         # create code is deleted
         form = CarouselModelForm(request.POST, request.FILES)
-        print(form)
-        
+        print(f"\n form \n")
+
         if form.is_valid():
             form.save()
 
         messages.success(request,"Bir şeyler eklendi ama ne oldu bilemiyorum")
     return render(request, "manage/carousel_create.html", context)
 
-def carousel_list(request):
-    context = dict()
-    # kaft_clone.com/manage/carousel/1/edit
-    context["item"] = Carousel.objects.get(pk=pk).order_by("-pk")
-    return render(request, "manage/carousel_list.html", context)
-
-def carousel_update(request, pk):
-    # Bu ismi oluşturun...
-    context = dict()
-    # kaft_clone/manage/carousel/1/edit
-    # item = Carousel.objects.first()
-    # context["form"] = CarouselModelForm(instance=item)
-    item = Carousel.objects.get(pk=pk)
-    context["form"] = CarouselModelForm(instance=item)
-    return render(request, "manage/corousel_form.html", context)
-
-    # stuff not checked
-    if form.is_valid():
-        form.save()
-    
-    messages.success(request, "manage/carousel_form.html", context)
 
